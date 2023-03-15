e1017ae3df7ab711135f693db6a638e4e7c0ee19
HappY-FaceS <rishavkumarcgc@gmail.com>
2023-03-15 21:33:23 +0530

    Added notification job

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { PORT,EMAIL_ID } = require('./config/serverConfig');
-// const { sendBasicEmail } = require('./services/email-service');
+const { PORT } = require('./config/serverConfig');
 
-const cron = require('node-cron');
+// const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
+
+const jobs = require('./utils/job');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create); // route for notification
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-        
+
+
+        jobs();
+
         // sendBasicEmail(
-        //     'EMAIL_ID',
-        //     'rishavkumarcgc12@gmail.com',
-        //     'Project : Safar',
+        //     'support@admin.com',
+        //     'moviebookingappservice@gmail.com',
+        //     'This is a testing email',
         //     'Hey, how are you, I hope you like the support'
         // );
-
-        // cron.schedule('*/1 * * * *', () => {
-         //   console.log('running a task every one minutes');
-        //  }); 
-          
-        
+              
     });
 }
 
